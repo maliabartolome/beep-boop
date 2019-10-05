1c3b6a2f06e157aa72b1d96732ccc91f1d878448
Malia Bartolome <maliabartolome@gmail.com>
2019-10-05 15:33:09 -0700

    added form input query

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+
+
+  if () {
+    $('#boop-container').show();
+    $('#beep-container, #beepboop-container').hide();
+    } else if () {
+      $('#beepboop-container').show();
+      $('#boop-container, #beep-container').hide();
+    } else {
+      $('#beep-container').show();
+      $('#boop-container, #beepboop-container').hide();
+
+
+$(document).ready(function(event) {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+  var usernameInput = $(".usernameInput").val();
+  var numberInput = $(".numberInput").val();
+    // console.log(formInput)
+  })
+
+
+})
