f97d886225f3d56e77601b21ea7e42cf427a5ff3
Malia Bartolome <maliabartolome@gmail.com>
2019-10-05 17:41:53 -0700

    completed query functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
+// business logic
 
 
 
-  if () {
-    $('#boop-container').show();
-    $('#beep-container, #beepboop-container').hide();
-    } else if () {
-      $('#beepboop-container').show();
-      $('#boop-container, #beep-container').hide();
-    } else {
-      $('#beep-container').show();
-      $('#boop-container, #beepboop-container').hide();
-
-
-$(document).ready(function(event) {
+// user interface logic
+$(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-  var usernameInput = $(".usernameInput").val();
   var numberInput = $(".numberInput").val();
-    // console.log(formInput)
+  var numberArray = []
+    for (var i = 0; i <= numberInput; i++) {
+      numberArray.push(i)
+    }
+    // console.log(numberArray)
+
+    var results = numberArray.map(function(number) {
+      // console.log(number)
+      var numberToString = number.toString();
+      // console.log(numberToString.indexOf(1))
+
+    if (numberToString.indexOf(3) > -1) {
+       return " I'm sorry -- Friend";
+     } else if (numberToString.indexOf(2) > -1) {
+       return " Boop";
+     } else if (numberToString.indexOf(1) > -1) {
+        return " Beep";
+      } else {
+        return " " + number;
+      }
+    })
+    // console.log(results);
+    $("#result").text(results);
+    $("form").hide();
   })
-
-
 })
